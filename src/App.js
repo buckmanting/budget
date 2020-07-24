7d808d6e582c5cef6e945d3845ed08a3d10e3ff7
Aaron Buckley <aaron.buckley@springernature.com>
2020-07-24 12:07:25 +0100

    adds something that works

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [budget, setBudget] = useState(100);
+    const [remaining, setRemaining] = useState(100);
+    const [spend, setSpend] = useState(0);
+    const [spendHistory, setSpendHistory] = useState([]);
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        setSpendHistory([...spendHistory, spend])
+        setSpend(0);
+    }
+
+    // Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+        // Update the document title using the browser API
+        // document.title = `You clicked ${spend} times`;
+        if (spendHistory.length) {
+            setRemaining(budget - spendHistory.reduce((total, curr) => total + curr));
+        }
+    });
+
+    return (
+        <div className="App">
+            <p className="App-header">You've got about £
+                <button className="budget-button--no-style">{remaining}</button>
+                left
+            </p>
+            <form onSubmit={handleSubmit}
+                  className="spend-form--extra-margin">
+                <label className="spend-label--small" htmlFor="spend">
+                    Spend
+                </label>
+                <input
+                    type="number"
+                    id="spend"
+                    onChange={e => setSpend(e.target.value)}
+                />
+                <input type="submit" className="spend-button--no-style" value="Add"/>
+            </form>
+            <ol>
+                {spendHistory.map((item) =>
+                    <li>£{item}</li>
+                )}
+            </ol>
+        </div>
+    );
 }
 
 export default App;
