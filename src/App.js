258a2b0299af5f4e11d217c21b56e916a3516af9
Aaron Buckley <aaron.buckley@springernature.com>
2020-07-25 13:51:58 +0100

    adds localstorage as a stopover and cache

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, {useEffect, useState} from 'react';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
 import './App.css';
+import ListGroup from "react-bootstrap/ListGroup";
+import useLocalStorage from './store/localStorageStore';
 
 function App() {
-    const [budget, setBudget] = useState(100);
-    const [remaining, setRemaining] = useState(budget);
-    const [spend, setSpend] = useState(0);
-    const [spendHistory, setSpendHistory] = useState([]);
+    const [budget, setBudget] = useLocalStorage('budget', {limit: 0, numberOfWeeks: 0});
+    const [remaining, setRemaining] = useLocalStorage('remaining', 0);
+    const [spend, setSpend] = useLocalStorage('spend', undefined);
+    const [spendHistory, setSpendHistory] = useLocalStorage('spendHistory', []);
+
+    const [showSettings, setShowSettings] = useState(false);
+    const [budgetLimit, setBudgetLimit] = useLocalStorage('budgetLimit', {limit: 100, numberOfWeeks: 4});
+    const [budgetTimeFrame, setBudgetTimeFrame] = useLocalStorage('budgetTime', {limit: 100, numberOfWeeks: 4});
+
+    const handleClose = () => setShowSettings(false);
+    const handleShow = () => setShowSettings(true);
 
     const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
 
-    const handleSubmit = (evt) => {
-        evt.preventDefault();
+    const handleSpendSubmit = (event) => {
+        event.preventDefault();
         setSpendHistory([...spendHistory, {
             value: Number.parseInt(spend),
             date: Date.now()
@@ -18,42 +35,119 @@ function App() {
         setSpend(0);
     }
 
+    const handleBudgetSubmit = (event) => {
+        event.preventDefault();
+
+        setBudget({
+            limit: budgetLimit,
+            numberOfWeeks: budgetTimeFrame
+        });
+
+        handleClose();
+    }
+
     useEffect(() => {
-        setRemaining(budget - spendHistory.reduce((a, b) => {
+        setRemaining((budget.limit / budget.numberOfWeeks) - spendHistory.reduce((a, b) => {
             return b.value == null ? a : a + b.value;
         }, 0));
         // save the budget here
     });
 
     return (
-        <div className="App">
-            <p className="App-header">
-                You've got about £{remaining} left
-            </p>
-            <form onSubmit={handleSubmit}>
-                <label className="spend-label--small" htmlFor="spend">
-                    Spend
-                </label>
-                <input
-                    type="number"
-                    id="spend"
-                    value={spend}
-                    className="form-input"
-                    onChange={e => setSpend(e.target.value)}
-                />
-                <input type="submit" className="spend-button--no-style" value="Add"/>
-            </form>
 
-            <p>
-                <button onClick={() => alert('i should do something')}>Configure</button>
-            </p>
+        <Container fluid className="App">
+            <Row className="App-header">
+                <Col sm={12}><h2>You've got about £{remaining} left</h2></Col>
+                <Col sm={12}><p>With £{budget.limit} until the end of the month</p></Col>
+            </Row>
+            <Form onSubmit={handleSpendSubmit}>
+                <InputGroup className="mb-3">
+                    <FormControl
+                        placeholder="£10"
+                        aria-label="Spent"
+                        aria-describedby="basic-addon2"
+                        value={spend}
+                        type="number"
+                        onChange={e => setSpend(e.target.value)}
+                    />
+                    <InputGroup.Append>
+                        <Button type="submit" variant="outline-primary">Add</Button>
+                    </InputGroup.Append>
+                </InputGroup>
 
-            <ol>
-                {spendHistory.map((item, key) =>
-                    <li key={item.date}>{new Date(item.date).toLocaleString('en-GB', options)} - £{item.value}</li>
-                )}
-            </ol>
-        </div>
+            </Form>
+
+            <Row>
+                <Col><Button variant="outline-secondary"
+                             onClick={handleShow}>Configure</Button></Col>
+            </Row>
+
+            <Row>
+                <Col>
+                    <ListGroup variant="flush">
+                        {spendHistory.map((item, key) =>
+                            <ListGroup.Item
+                                className="component--dark-bg"
+                                key={item.date}>{new Date(item.date).toLocaleString('en-GB', options)} -
+                                £{item.value}</ListGroup.Item>
+                        )}
+                    </ListGroup>
+                </Col>
+            </Row>
+
+            <Modal show={showSettings} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Configure Budget</Modal.Title>
+                </Modal.Header>
+
+                <Form onSubmit={handleBudgetSubmit}>
+                    <Modal.Body>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Budget</Form.Label>
+                            <InputGroup>
+                                <InputGroup.Prepend>
+                                    <InputGroup.Text id="budgetPrepend">£</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <Form.Control type="number"
+                                              value={budgetLimit}
+                                              onChange={(e) => setBudgetLimit(e.target.value)}
+                                              placeholder="100" required/>
+                                <Form.Control.Feedback type="invalid">
+                                    Please set a budget.
+                                </Form.Control.Feedback>
+                            </InputGroup>
+                            <Form.Text className="text-muted">
+                                This for the time frame below
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Over</Form.Label>
+                            <InputGroup>
+                                <Form.Control type="number"
+                                              value={budgetTimeFrame}
+                                              onChange={(e) => setBudgetTimeFrame(e.target.value)}
+                                              placeholder="1" required/>
+                                <InputGroup.Append>
+                                    <InputGroup.Text id="timeFrameAppend">Weeks</InputGroup.Text>
+                                </InputGroup.Append>
+                                <Form.Control.Feedback type="invalid">
+                                    Please set a time frame.
+                                </Form.Control.Feedback>
+                            </InputGroup>
+                            <Form.Text className="text-muted">
+                                This is in weeks
+                            </Form.Text>
+                        </Form.Group>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>Close</Button>
+                        <Button variant="primary" type="submit">Save changes</Button>
+                    </Modal.Footer>
+                </Form>
+            </Modal>
+        </Container>
     );
 }
 
